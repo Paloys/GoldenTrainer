e7ee49ff8c65a113c547265f6e1f043b9304be2c
Paloys <louis300305.gaming@gmail.com>
2022-03-13 17:47:22 +0100

    First real commit

---
+++ GoldenTrainer/ExampleModuleSettings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Input;
+using YamlDotNet.Serialization;
+
+namespace Celeste.Mod.Example
+{
+    public class ExampleModuleSettings : EverestModuleSettings
+    {
+        // SettingName also works on props, defaulting to
+        // modoptions_[typename without settings]_[propname]
+
+        // Example ON / OFF property with a default value.
+        public bool ModActivate { get; set; } = false;
+
+        [SettingRange(1, 10)]
+        public int NumberOfCompletions { get; set; } = 5;
+
+
+    }
+}
---
+++ GoldenTrainer/GoldenTrainer.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+
+
+namespace Celeste.Mod.Example
+{
+    public class GoldenTrainer : EverestModule
+    {
+        // Only one alive module instance can exist at any given time.
+        public static GoldenTrainer Instance;
+
+        public GoldenTrainer()
+        {
+            Instance = this;
+        }
+
+        // If you need to store settings:
+        public override Type SettingsType => typeof(ExampleModuleSettings);
+        public static ExampleModuleSettings Settings => (ExampleModuleSettings)Instance._Settings;
+
+        public int CompletionCount { get; set; } = 0;
+
+
+        // Initialized in LoadContent, after graphics and other assets have been loaded.
+        public SpriteBank ExampleSpriteBank;
+
+        // Set up any hooks, event handlers and your mod in general here.
+        // Load runs before Celeste itself has initialized properly.
+        public override void Load()
+        {
+            // SetLogLevel will set the *minimum* log level that will be written for logs that use the given prefix string.
+            Logger.SetLogLevel("GoldenTrainer", LogLevel.Verbose);
+            Logger.Log(LogLevel.Info, "GoldenTrainer", "Loading GoldenTrainer Hooks");
+            // The default LogLevel when using Logger.Log is Verbose.
+            Logger.Log(LogLevel.Verbose, "GoldenTrainer", "This line would not be logged with SetLogLevel LogLevel.Info");
+            Everest.Events.Level.OnTransitionTo += RespawnAtEnd;
+        }
+        
+
+            // Optional, initialize anything after Celeste has initialized itself properly.
+        public override void Initialize()
+        {
+        }
+
+        // Unload the entirety of your mod's content. Free up any native resources.
+        public override void Unload()
+        {
+
+        }
+
+        private void RespawnAtEnd(Level level, LevelData next, Vector2 direction)
+        {
+            if (Settings.ModActivate)
+            {
+                Instance.CompletionCount++;
+                if (Instance.CompletionCount < Settings.NumberOfCompletions)
+                {
+                    Player p = level.Tracker.GetEntity<Player>();
+                    p.Die(p.Position, true, false);
+                }
+                else if (Instance.CompletionCount == Settings.NumberOfCompletions)
+                {
+                    Instance.CompletionCount = 0;
+                }
+            }
+        }
+            
+    }
+}
